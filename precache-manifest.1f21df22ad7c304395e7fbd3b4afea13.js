2c04abef884acb23253b543565fdb64835d6f3cf
Michael Bauer <michael@m-bauer.org>
2020-02-12 16:57:54 +0100

    Updates

---
+++ precache-manifest.1f21df22ad7c304395e7fbd3b4afea13.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c703b7a82e42a6b8a7dfad64dab95153",
+    "url": "/token-allowance-checker/index.html"
+  },
+  {
+    "revision": "9557688379c89b8a19e6",
+    "url": "/token-allowance-checker/static/css/2.7e9d59df.chunk.css"
+  },
+  {
+    "revision": "74fdf55554d07104f2c2",
+    "url": "/token-allowance-checker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "584f3b81bae1357a9752",
+    "url": "/token-allowance-checker/static/js/10.049e8d37.chunk.js"
+  },
+  {
+    "revision": "4e8fe9a3892013102d6c",
+    "url": "/token-allowance-checker/static/js/11.e0c570d3.chunk.js"
+  },
+  {
+    "revision": "35a2646f65afe7d481d2",
+    "url": "/token-allowance-checker/static/js/12.9054f93f.chunk.js"
+  },
+  {
+    "revision": "03f1c04ff69f17a2dab2",
+    "url": "/token-allowance-checker/static/js/13.2804e66b.chunk.js"
+  },
+  {
+    "revision": "d9755231b2715008f3b5",
+    "url": "/token-allowance-checker/static/js/14.a317bb21.chunk.js"
+  },
+  {
+    "revision": "d828d064fd611903189974745a0859f2",
+    "url": "/token-allowance-checker/static/js/14.a317bb21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70695e78c2c60f20fd5a",
+    "url": "/token-allowance-checker/static/js/15.80abd110.chunk.js"
+  },
+  {
+    "revision": "091da06c11faa4786ff98801cf526ad7",
+    "url": "/token-allowance-checker/static/js/15.80abd110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424ba47b2fb0a66c1684",
+    "url": "/token-allowance-checker/static/js/16.55a9cf4f.chunk.js"
+  },
+  {
+    "revision": "7e5b0c93619b8e247b63",
+    "url": "/token-allowance-checker/static/js/17.6ccd534c.chunk.js"
+  },
+  {
+    "revision": "efce90196036178a3487",
+    "url": "/token-allowance-checker/static/js/18.1248a376.chunk.js"
+  },
+  {
+    "revision": "7f98e9fd3494c3130b5e",
+    "url": "/token-allowance-checker/static/js/19.0c1331f0.chunk.js"
+  },
+  {
+    "revision": "9557688379c89b8a19e6",
+    "url": "/token-allowance-checker/static/js/2.db1a8f36.chunk.js"
+  },
+  {
+    "revision": "eb0a49be8cfd16805b20a6b8e4e6714c",
+    "url": "/token-allowance-checker/static/js/2.db1a8f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f2f995d7c80d0bfba6",
+    "url": "/token-allowance-checker/static/js/20.509a5680.chunk.js"
+  },
+  {
+    "revision": "bd76dce56f3310647b76",
+    "url": "/token-allowance-checker/static/js/21.bd01ff29.chunk.js"
+  },
+  {
+    "revision": "2b5ebcf05a59e181edb2",
+    "url": "/token-allowance-checker/static/js/22.736687e4.chunk.js"
+  },
+  {
+    "revision": "ceee1bc5a9796e3f5316",
+    "url": "/token-allowance-checker/static/js/23.c370a224.chunk.js"
+  },
+  {
+    "revision": "6f00dbec668dee5bc0f5",
+    "url": "/token-allowance-checker/static/js/24.63697e4f.chunk.js"
+  },
+  {
+    "revision": "6cd0242c77836be9d431",
+    "url": "/token-allowance-checker/static/js/25.22977300.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/token-allowance-checker/static/js/25.22977300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c48624e39197f4e857",
+    "url": "/token-allowance-checker/static/js/3.2e5aa679.chunk.js"
+  },
+  {
+    "revision": "771f956a07956691f3be8ea5d94aa9dd",
+    "url": "/token-allowance-checker/static/js/3.2e5aa679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7515b3293b802ad4edbc",
+    "url": "/token-allowance-checker/static/js/4.1bb653a1.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/token-allowance-checker/static/js/4.1bb653a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f5a972f7aefd7e9a80",
+    "url": "/token-allowance-checker/static/js/5.0fc8d3cf.chunk.js"
+  },
+  {
+    "revision": "b2f4755df921e9216fa6",
+    "url": "/token-allowance-checker/static/js/6.81958507.chunk.js"
+  },
+  {
+    "revision": "9d7f87f96a8ab747d7d1",
+    "url": "/token-allowance-checker/static/js/7.4a9945cc.chunk.js"
+  },
+  {
+    "revision": "cafcb8d473b0c0fcad0c",
+    "url": "/token-allowance-checker/static/js/8.84489fc2.chunk.js"
+  },
+  {
+    "revision": "f8b2b999b111908090c0",
+    "url": "/token-allowance-checker/static/js/9.78751b6f.chunk.js"
+  },
+  {
+    "revision": "74fdf55554d07104f2c2",
+    "url": "/token-allowance-checker/static/js/main.46eb39cc.chunk.js"
+  },
+  {
+    "revision": "12e24e262f684b485a0d",
+    "url": "/token-allowance-checker/static/js/runtime-main.e06a46a2.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/token-allowance-checker/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/token-allowance-checker/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/token-allowance-checker/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/token-allowance-checker/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/token-allowance-checker/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/token-allowance-checker/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/token-allowance-checker/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/token-allowance-checker/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/token-allowance-checker/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/token-allowance-checker/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/token-allowance-checker/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/token-allowance-checker/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/token-allowance-checker/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/token-allowance-checker/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/token-allowance-checker/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/token-allowance-checker/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e7b89307a0622add7b0b6f8bc14e3c7.js"
+  "/token-allowance-checker/precache-manifest.1f21df22ad7c304395e7fbd3b4afea13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/token-allowance-checker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
