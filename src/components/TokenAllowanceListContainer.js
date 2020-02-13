3218caf31737d78b01d41ec136def4bda72d7014
Michael Bauer <michael@m-bauer.org>
2020-02-13 16:54:44 +0100

    Add infrastructure to support non-ERC20 compliant tokens. Add MKR and SAI to wellKnownContracts.js.

--- src/components/TokenAllowanceListContainer.js
+++ src/components/TokenAllowanceListContainer.js
@@ -3,8 +3,10 @@ import ERC20Data from '@openzeppelin/contracts/build/contracts/ERC20Detailed.jso
 import {Web3Context} from './OnboardContext'
 import PropTypes from 'prop-types'
 import TokenAllowanceItem from './TokenAllowanceItem'
+import wellKnownContracts from './wellKnownContracts'
 const contract = require('@truffle/contract')
 const namehash = require('eth-ens-namehash')
+
 
 /*
     TokenAllowanceListContainer:
@@ -33,6 +35,23 @@ const TokenAllowanceListContainer = ({contractAddress, owner, spenders}) => {
             const erc20Contract = contract(ERC20Data)
             erc20Contract.setProvider(web3Context.web3.currentProvider)
             const contractInstance = await erc20Contract.at(contractAddress)
+            let tokenName = ''
+            let tokenSymbol = ''
+            // Some contracts like MKR and SAI do not implement the correct ERC20 name and symbol.
+            // Get their data from hardocded fallback
+            if (Object.keys(wellKnownContracts[web3Context.networkId]).includes(contractAddress)) {
+                tokenName = wellKnownContracts[web3Context.networkId][contractAddress].name
+                tokenSymbol = wellKnownContracts[web3Context.networkId][contractAddress].symbol
+            } else {
+                try {
+                    tokenName = await contractInstance.name()
+                    tokenSymbol = await contractInstance.symbol()
+                } catch(error) {
+                    // Most likely token contract does not implement the name() method. Ignore error.
+                    console.warn(`Failed to get name/symbol of contract at ${contractAddress}. Please raise
+                    an issue to add this token at https://github.com/TripleSpeeder/token-allowance-checker/issues!`)
+                }
+            }
             const decimals = await contractInstance.decimals()
             const totalSupply = await contractInstance.totalSupply()
             const balance = await contractInstance.balanceOf(owner)
@@ -42,13 +61,8 @@ const TokenAllowanceListContainer = ({contractAddress, owner, spenders}) => {
             setTokenDecimals(decimals)
             setTokenSupply(totalSupply)
             setOwnerBalance(balance)
-            try {
-                setTokenName(await contractInstance.name())
-                setTokenSymbol(await contractInstance.symbol())
-            } catch(error) {
-                // Most likely token contract does not implement the name() method. Ignore error.
-                console.log(`Failed to get name/symbol of contract at ${contractAddress}`)
-            }
+            setTokenName(tokenName)
+            setTokenSymbol(tokenSymbol)
 
             // for each allowedAddress, get current allowance of spender
             const allowances = {}
