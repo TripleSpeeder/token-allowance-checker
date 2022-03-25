50240c5725ca24d9098249ade060562c7b212164
Michael Bauer <michael@m-bauer.org>
2022-03-25 10:03:32 +0100

    refactor: mobile layout with latest semantic-ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,105 +1,100 @@
 import React from 'react'
 import { HashRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import {
-    Button,
-    Container,
-    Grid,
-    Header,
-    Icon,
-    Segment,
+  Button,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Segment
 } from 'semantic-ui-react'
 import AllowanceLister from '../features/allowancesList/AllowanceLister'
 import OnboardGate from '../features/onboard/OnboardGate'
 import AddressInputContainer from '../features/addressInput/AddressInputContainer'
 import AddressExtractor from '../components/AddressExtractor'
-import { useSelector } from 'react-redux'
 import { RootState } from './rootReducer'
 import ResponsiveApp from './ResponsiveApp'
 import Footer from './Footer'
 import { useAppSelector } from './hooks'
 
-const App: React.FC = () => {
-    const { mobile } = useAppSelector((state: RootState) => state.respsonsive)
+const App = () => {
+  const { mobile } = useAppSelector((state: RootState) => state.respsonsive)
 
-    const size = mobile ? 'small' : 'huge'
+  const size = mobile ? 'small' : 'huge'
 
-    const startButton = (
-        <Segment basic textAlign='center'>
-            <Button primary as={Link} to='/address/' size='massive'>
-                Check Allowances
-                <Icon name='arrow right' />
-            </Button>
-        </Segment>
-    )
+  const startButton = (
+    <Segment basic textAlign='center'>
+      <Button primary as={Link} to='/address/' size='massive'>
+        Check Allowances
+        <Icon name='arrow right' />
+      </Button>
+    </Segment>
+  )
 
-    return (
-        <Router>
-            <ResponsiveApp>
-                <Routes>
-                    <Route path='/address/:address'>
-                        <Container>
-                            <OnboardGate>
-                                <AddressExtractor>
-                                    <AddressInputContainer />
-                                    <AllowanceLister />
-                                </AddressExtractor>
-                            </OnboardGate>
-                        </Container>
-                    </Route>
-                    <Route path='/'>
-                        {startButton}
-                        <Segment basic vertical size={size}>
-                            <Grid container stackable verticalAlign='top'>
-                                <Grid.Row>
-                                    <Grid.Column width={8}>
-                                        <Header>Control your approvals</Header>
-                                        <p>
-                                            <em>Token Allowance Checker</em>{' '}
-                                            shows all approvals for your
-                                            ERC20-compliant tokens, and the
-                                            option to change the approved amount
-                                            - or completely zero it.
-                                        </p>
-                                    </Grid.Column>
-                                    <Grid.Column width={8}>
-                                        <Header>
-                                            The unlimited approval problem
-                                        </Header>
-                                        <p>
-                                            Many DApps have the habit of
-                                            requiring you to approve effectively
-                                            unlimited amount of tokens. This
-                                            helps improving the user experience,
-                                            as you only have to sign off an
-                                            approval once and it will be enough
-                                            for all future transactions.
-                                        </p>
-                                        <p>
-                                            However this also means that the
-                                            DApp (or the person/entity
-                                            controlling it) can at any time
-                                            transfer <em>all of your tokens</em>
-                                            , without requiring any further
-                                            approval.
-                                        </p>
-                                        <p>
-                                            In addition, there is no concept of
-                                            expiring approvals. Once approved,
-                                            the approval will remain forever. If
-                                            you do not trust a DApp or its
-                                            operators anymore, there is usually
-                                            no easy way to remove the approval.
-                                        </p>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Segment>
-                    </Route>
-                </Routes>
-                <Footer />
-            </ResponsiveApp>
-        </Router>
-    )
+  const addrElem = (
+    <Container>
+      <OnboardGate>
+        <AddressExtractor>
+          <AddressInputContainer />
+          <AllowanceLister />
+        </AddressExtractor>
+      </OnboardGate>
+    </Container>
+  )
+
+  const rootElem = (
+    <>
+      {startButton}
+      <Segment basic vertical size={size}>
+        <Grid container stackable verticalAlign='top'>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header>Control your approvals</Header>
+              <p>
+                <em>Token Allowance Checker</em> shows all approvals for your
+                ERC20-compliant tokens, and the option to change the approved
+                amount - or completely zero it.
+              </p>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header>The unlimited approval problem</Header>
+              <p>
+                Many DApps have the habit of requiring you to approve
+                effectively unlimited amount of tokens. This helps improving the
+                user experience, as you only have to sign off an approval once
+                and it will be enough for all future transactions.
+              </p>
+              <p>
+                However this also means that the DApp (or the person/entity
+                controlling it) can at any time transfer{' '}
+                <em>all of your tokens</em>, without requiring any further
+                approval.
+              </p>
+              <p>
+                In addition, there is no concept of expiring approvals. Once
+                approved, the approval will remain forever. If you do not trust
+                a DApp or its operators anymore, there is usually no easy way to
+                remove the approval.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </>
+  )
+
+  return (
+    <Router>
+      <ResponsiveApp>
+        <Routes>
+          <Route path='/address/:address' element={addrElem} />
+          <Route path='/address' element={addrElem} />
+          <Route path='/' element={rootElem} />
+        </Routes>
+        <Footer />
+      </ResponsiveApp>
+    </Router>
+  )
 }
 
 export default App
--- src/app/ResponsiveApp.tsx
+++ src/app/ResponsiveApp.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { RootState } from './rootReducer'
-import { ResponsiveOnUpdateData } from 'semantic-ui-react/dist/commonjs/addons/Responsive/Responsive'
 import {
-    Container,
-    Header,
-    Icon,
-    Image,
-    Menu,
-    Segment,
-    Sidebar,
-    Button,
+  Container,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+  Button
 } from 'semantic-ui-react'
 import { setMobile } from '../features/responsiveLayout/responsiveSlice'
-import { HashRouter as Router, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import MainMenu from '../features/menu/MainMenu'
 import WalletSelectorContainer from '../features/onboard/WalletSelectorContainer'
 import NetworkSelector from '../components/NetworkSelector'
@@ -21,137 +20,138 @@ import { initialize } from '../features/onboard/onboardSlice'
 import { useNavigate } from 'react-router'
 import { useAppSelector } from './hooks'
 
+const testIsMobile = () => {
+  if (typeof window === 'undefined') {
+    return true
+  }
+  return window.innerWidth < 768
+}
+
 interface ResponsiveAppProps {
-    children?: React.ReactNode
+  children?: React.ReactNode
 }
 const ResponsiveApp = ({ children }: ResponsiveAppProps) => {
-    const [prevMobile, setPrevMobile] = useState<boolean | undefined>(undefined)
-    const [showSidebar, setShowSidebar] = useState(false)
-    const { mobile } = useAppSelector((state: RootState) => state.respsonsive)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const [prevMobile, setPrevMobile] = useState<boolean | undefined>(undefined)
+  const [showSidebar, setShowSidebar] = useState(false)
+  const { mobile } = useAppSelector((state: RootState) => state.respsonsive)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    // initialize onboard.js
-    useEffect(() => {
-        dispatch(initialize(navigate))
-    }, [dispatch, navigate])
+  // initialize onboard.js
+  useEffect(() => {
+    dispatch(initialize(navigate))
+  }, [dispatch, navigate])
 
-    const onResponsiveUpdate = (
-        event: React.SyntheticEvent<HTMLElement>,
-        data: ResponsiveOnUpdateData
-    ) => {
-        // @ts-ignore
-        const isMobile = data.width <= Responsive.onlyMobile.maxWidth
-        if (isMobile !== prevMobile) {
-            dispatch(setMobile(isMobile))
-            setPrevMobile(isMobile)
-        }
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return
     }
 
-    const DesktopHeading = (
-        <Segment textAlign='center' vertical basic>
-            <MainMenu />
-            <Container
-                text
-                style={{
-                    marginTop: '4em',
-                    marginBottom: '2em',
-                }}
-            >
-                <Header
-                    as='h1'
-                    inverted={false}
-                    style={{
-                        fontSize: '3em',
-                        marginTop: '0.5em',
-                    }}
-                >
-                    Token Allowance Checker
-                    <Header.Subheader>
-                        Control your token approvals
-                    </Header.Subheader>
-                </Header>
-            </Container>
-        </Segment>
-    )
-
-    let content
-    if (mobile) {
-        // No extra page header, use sidebar for menu
-        content = (
-            <Sidebar.Pushable>
-                <Menu fixed='top' inverted size='small'>
-                    <Menu.Item onClick={() => setShowSidebar(true)}>
-                        <Icon name={'bars'} />
-                    </Menu.Item>
-                    <Menu.Item style={{ padding: '0' }}>
-                        <Link to={'/'}>
-                            <Image
-                                src={'/logo192.png'}
-                                width={40}
-                                height={40}
-                            />
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item>Token Allowance Checker</Menu.Item>
-                </Menu>
-                <Sidebar
-                    as={Menu}
-                    animation={'overlay'}
-                    inverted
-                    onHide={() => setShowSidebar(false)}
-                    vertical
-                    visible={showSidebar}
-                >
-                    <Menu.Item>
-                        <WalletSelectorContainer />
-                    </Menu.Item>
-                    <Menu.Item>
-                        <NetworkSelector />
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Button
-                            icon
-                            fluid
-                            as={'a'}
-                            href='https://github.com/TripleSpeeder/token-allowance-checker'
-                            title='github.com/TripleSpeeder/token-allowance-checker'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            <Icon name='github' /> TAC on Github
-                        </Button>
-                    </Menu.Item>
-                </Sidebar>
-
-                <Sidebar.Pusher style={{ marginTop: '3em' }}>
-                    {children}
-                </Sidebar.Pusher>
-            </Sidebar.Pushable>
-        )
-    } else {
-        // use page header with classic menu
-        content = (
-            <>
-                {DesktopHeading}
-                {children}
-            </>
-        )
+    function autoResize() {
+      const isMobile = testIsMobile()
+      if (isMobile !== prevMobile) {
+        console.log(`Setting isMobile ${isMobile}`)
+        dispatch(setMobile(isMobile))
+        setPrevMobile(isMobile)
+      }
     }
 
-    return <Router>{content}</Router>
-    /*
-    return (
-        <Responsive
-            as={Router}
-            fireOnMount={true}
-            onUpdate={onResponsiveUpdate}
+    window.addEventListener('resize', autoResize)
+
+    // This is likely unnecessary, as the initial state should capture
+    // the size, however if a resize occurs between initial state set by
+    // React and before the event listener is attached, this
+    // will just make sure it captures that.
+    autoResize()
+
+    // Return a function to disconnect the event listener
+    return () => window.removeEventListener('resize', autoResize)
+  }, [dispatch, prevMobile])
+
+  const DesktopHeading = (
+    <Segment textAlign='center' vertical basic>
+      <MainMenu />
+      <Container
+        text
+        style={{
+          marginTop: '4em',
+          marginBottom: '2em'
+        }}
+      >
+        <Header
+          as='h1'
+          inverted={false}
+          style={{
+            fontSize: '3em',
+            marginTop: '0.5em'
+          }}
         >
-            {content}
-        </Responsive>
-    )
+          Token Allowance Checker
+          <Header.Subheader>Control your token approvals</Header.Subheader>
+        </Header>
+      </Container>
+    </Segment>
+  )
 
- */
+  let content
+  if (mobile) {
+    // No extra page header, use sidebar for menu
+    content = (
+      <Sidebar.Pushable>
+        <Menu fixed='top' inverted size='small'>
+          <Menu.Item onClick={() => setShowSidebar(true)}>
+            <Icon name={'bars'} />
+          </Menu.Item>
+          <Menu.Item style={{ padding: '0' }}>
+            <Link to={'/'}>
+              <Image src={'/logo192.png'} width={40} height={40} />
+            </Link>
+          </Menu.Item>
+          <Menu.Item>Token Allowance Checker</Menu.Item>
+        </Menu>
+        <Sidebar
+          as={Menu}
+          animation={'overlay'}
+          inverted
+          onHide={() => setShowSidebar(false)}
+          vertical
+          visible={showSidebar}
+        >
+          <Menu.Item>
+            <WalletSelectorContainer />
+          </Menu.Item>
+          <Menu.Item>
+            <NetworkSelector />
+          </Menu.Item>
+          <Menu.Item>
+            <Button
+              icon
+              fluid
+              as={'a'}
+              href='https://github.com/TripleSpeeder/token-allowance-checker'
+              title='github.com/TripleSpeeder/token-allowance-checker'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <Icon name='github' /> TAC on Github
+            </Button>
+          </Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher style={{ marginTop: '3em' }}>{children}</Sidebar.Pusher>
+      </Sidebar.Pushable>
+    )
+  } else {
+    // use page header with classic menu
+    content = (
+      <>
+        {DesktopHeading}
+        {children}
+      </>
+    )
+  }
+
+  return content
 }
 
 export default ResponsiveApp
